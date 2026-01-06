b7a25df5de903c2e8915db6e56f176e133a65eb1
masare <asaremark.ma@gmail.com>
2026-01-06 18:02:05 +0000

    Updated code for task 2 of day 3: Still not giving the right answer

--- 2025/Day_3_Lobby/task2_2.py
+++ 2025/Day_3_Lobby/task2_2.py
@@ -1,29 +1,55 @@
-from itertools import combinations, repeat
+from itertools import combinations
 from typing import Generator
 import numpy as np
 
-BANKS_TXT: str = "test.txt"
-# BANKS_TXT: str = "input.txt"
+# BANKS_TXT: str = "test.txt"
+BANKS_TXT: str = "input.txt"
 
 def bank_battery_finder(bank: str) -> str:
     # print(bank, '------')
-    # b_len: int = len(bank)
-    best_pair: list[int] = [*repeat(0, 12)] # stores positions of best battery combo
-    max_cap: str = '000000000000' # stores max battery capacity for bank
-    bank_l = list(bank)
-    for i in range(12):
-        hbi: int = np.argmax(np.array(bank_l))
-        best_pair[i] = hbi
-        bank_l[hbi] = '0'
+    b_len: int = len(bank)
+    # best_pair: tuple = (1,2) # stores positions of best battery combo
+    max_cap: str = bank[:12]#''.join('0' for i in range(len(bank))) # stores max battery capacity for bank
 
-    
-    max_cap = ''.join(bank[i] for i in sorted(best_pair)) 
+    bank_str: list[str] = ['0' for i in range(b_len)]
+    bank_copy: list[str] = [i for i in bank]
+    hbi: int = bank_copy.index(max(bank_copy))
 
-    print(bank, max_cap)
+        
+    if hbi == b_len-1 or hbi == 0:
+        bank_str[hbi] = bank[hbi]
+        bank_copy[hbi] = '0'
+        for i in range(11):
+            hbi = bank_copy.index(max(bank_copy))
+            bank_str[hbi] = bank[hbi]
+            bank_copy[hbi] = '0'
+    elif len(bank[hbi:]) > 11:
+        bank_str = bank_str[hbi:]
+        bank_copy = bank_copy[hbi:]
+        for i in range(11):
+            hbi = bank_copy.index(max(bank_copy))
+            bank_str[hbi] = bank_copy[hbi]
+            bank_copy[hbi] = '0'
+    elif len(bank[hbi:]) < 12:
+        init_len = len(bank[hbi:])
+        n_iter: int = 12 - init_len
+        bank_str[hbi:] = bank_copy[hbi:]
+        bank_copy[hbi:] = ['0' for i in range(init_len)]
+        # print(init_len, n_iter, bank[hbi:], bank_str)
+        for i in range(n_iter):
+            hbi = bank_copy.index(max(bank_copy))
+            bank_str[hbi] = bank_copy[hbi]
+            bank_copy[hbi] = '0'
 
+
+    # print(bank_str)
+    # print(''.join(i for i in bank_str if i > '0'))
+
+
+    max_cap = ''.join(i for i in bank_str if i > '0')
+    # max_cap = max(bat_comb)    
+    # print(bank, bat_comb, max(bat_comb))
     # print(bank, hbi, best_pair, max_cap)
-    
-
     return max_cap
 
 def read_file(file_path) -> Generator[str, None, None]:
